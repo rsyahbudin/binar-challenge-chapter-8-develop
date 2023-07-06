478ce3b0be1e6f626af7b56f3125bfab74d65fa7
rsyahbudin <syahbudin45@gmail.com>
2023-07-06 14:53:52 +0700

    add swagger js doc

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const express = require('express')
+const expressJSDocSwagger = require('express-jsdoc-swagger')
 const app = express()
 const cors = require('cors')
 const apiRouter = require('./server/routes')
 const errorHandler = require('./server/middlewares/errorHandler')
+const swagger = require('./swagger')
 const PORT = process.env.PORT || 4000
+
+expressJSDocSwagger(app)(swagger);
 
 // middlewares
 app.use(cors())
---
+++ swagger.js
@@ -0,0 +1,25 @@
+module.exports = {
+    info: {
+        title: "Chapter 8 Binar",
+        description: "This is a documentation for learning purpose",
+        contact: {
+            name: 'team',
+            email: "myteam@binar.org"
+        },
+        version: "1.0.0"
+    },
+    servers: [
+        {
+            url: "http://localhost:5009/api/v1"
+        }
+        ],
+        tags: [
+        {
+            name: "Player",
+            description: "player endpoints"
+        }
+    ],
+    filesPattern: './app.js',
+    baseDir: __dirname,
+    swaggerUIPath: '/swagger',
+};
